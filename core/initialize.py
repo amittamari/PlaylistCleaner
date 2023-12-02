812858381d9b73c825574fe182bf68024960d505
Amit Tamari <15573913+amittamari@users.noreply.github.com>
2023-12-02 19:36:41 +0200

    raise original exception when theres no match

--- core/initialize.py
+++ core/initialize.py
@@ -15,9 +15,11 @@ def initialize_datasources() -> list[AnyDataSource]:
             for ds_type_class in DATASOURCES_TYPES
             for ds in ds_type_class.__subclasses__()
         ]
+
         return initialized_datasources
     except TypeError as e:
         m = regex.match(ABSTRACT_INSTANTIATE_ERROR_REGEX, str(e))
         if m is not None:
             raise NotImplementedError(m.group(1))
 
+        raise e
